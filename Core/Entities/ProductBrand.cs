0a1a6b954102fcba5973366092f08069795c760b
KhongVinh <acex123456789@gmail.com>
2020-07-01 11:52:03 +0700

    add Repository Pattern, Extending the products entity and creating related entities, Eager loading

---
+++ Core/Entities/ProductBrand.cs
@@ -0,0 +1,7 @@
+namespace Core.Entities
+{
+    public class ProductBrand : BaseEntities
+    {
+        public string Name {get; set; }
+    }
+}
---
+++ Core/Entities/ProductType.cs
@@ -0,0 +1,7 @@
+namespace Core.Entities
+{
+    public class ProductType : BaseEntities
+    {
+        public string Name { get; set; }
+    }
+}
